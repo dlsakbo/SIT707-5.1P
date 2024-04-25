ccd753ef3493343fd05f96604d5e0d8a152136d2
Sithira Akbo <56906001+dlsakbo@users.noreply.github.com>
2024-04-25 19:42:23 +1000

    main

--- src/test/java/sit707_week5/WeatherControllerTest.java
+++ src/test/java/sit707_week5/WeatherControllerTest.java
@@ -52,13 +52,13 @@ public class WeatherControllerTest {
 
     @Test
 	public void testStudentIdentity() {
-		String studentId = "223656754";
+		String studentId = "223057589";
 		Assert.assertNotNull("Student ID is null", studentId);
 	}
 
 	@Test
 	public void testStudentName() {
-		String studentName = "Upeksha Dilshan";
+		String studentName = "Sithira Akbo";
 		Assert.assertNotNull("Student name is null", studentName);
 	}
 
